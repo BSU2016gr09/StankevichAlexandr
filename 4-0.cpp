814ecf3122e5d391cf3e31ab9c980d149bad6579
StankevichAlexandr <graver1999@gmail.com>
2016-11-08 19:08:49 +0300

    homework4

---
+++ 4-0.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+int main()
+{
+	float x = -6, b;
+	while (x < 6) {
+		if (x == 0) b = 2; else b = (sin(2*x) / x) * 45;
+		cout << right << setw(b + 40) << '*' << "\n";
+		x += 0.1;
+	}
+	system("pause");
+}
