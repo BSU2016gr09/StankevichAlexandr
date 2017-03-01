f18afb4cbc6187a83d034d40bd86f4f74034b985
Alexandr <graver1999@gmail.com>
2017-03-01 19:32:40 +0300

    hw2

---
+++ 2-2.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+#include<ctime>
+using namespace std; //Реализуйте алгоритм XORShift нахождения псевдослучайных чисел в качестве отдельной функции, чтобы потом выполнять задания на массивы(там где надо случайными числами заполнить массив)!
+	try{
+		arr = new int[N];
+	}
+	catch (...){
+		cout << "Not enough memory";
+	}
+}
+
+void printArray(int* arr, int N){ //функция вывода массива на консоль
+	int j = 0;
+	while (j < N){
+		cout << arr[j++] << " ";
+	}
+}
+
+void xorSHIFT(int* arr, int k){
+	int i = 1, tmp;
+	arr[0] = k;
+	while (arr[i]){
+		tmp ^= arr[i - 1] << 13;
+		tmp ^= tmp >> 15;
+		arr[i] ^= tmp << 5;
+		i++;
+	}
+
+
+}
+
+int main(){ //Реализуйте алгоритм XORShift нахождения псевдослучайных чисел в качестве отдельной функции, чтобы потом выполнять задания на массивы (там где надо случайными числами заполнить массив)!
+	int N, k;
+	cin >> N >> k;
+	int* Array;
+	giveMemory(Array, N);
+	xorSHIFT(Array, k);
+	printArray(Array, N);
+	delete[] Array;
+	system("pause");
+}
